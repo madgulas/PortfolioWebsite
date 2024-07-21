f50463c3eef70553ae40ae391bfb1e0415ac7dc4
Madgula <madgul.siddu19@gmail.com>
2024-07-21 22:50:09 +0530

    intial2.2

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 gsap.to("#right-nav", {
-    y: 10,
+    y: -10,
     opacity: 100,
-    delay: 0.5,
+    delay: 0.9,
     duration: 1.5,
     repeat: -1,
     yoyo: true
+})
+gsap.from("#left-nav",{
+    delay: 1,
+    opacity: 0,
+    duration: 1,
+    y: -50,
+    // repeat: -1,
+    // yoyo: true
+})
+gsap.from("#left-nav h2",{
+    opacity: 0,
+    y: -50,
+    delay: 1,
+    duration: 1,
+    
 })
